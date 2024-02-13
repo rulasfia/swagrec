e8edc09074351cb3cf23436eda51991e7f2db61c
Khoirul Asfian <mk.asfian@gmail.com>
2024-02-13 22:36:40 +0700

    refactor: :recycle: refactor module part 1

--- bin/main.js
+++ bin/main.js
@@ -7,9 +7,6 @@ import { hideBin } from "yargs/helpers";
 import { request } from "undici";
 import { ValiError, parse, string, url } from "valibot";
 import { multiselect, question, select } from "@topcli/prompts";
-import { validateResponseFormat } from "./validate-response-format.js";
-import { exitWithError } from "./exit-with-error.js";
-import { readOrCreateOutputFile } from "./read-or-create-output.js";
 
 (async function main() {
 	const argv = yargs(hideBin(process.argv))
@@ -72,77 +69,29 @@ import { readOrCreateOutputFile } from "./read-or-create-output.js";
 				output_location = `${output_location}.json`;
 			}
 
+			/** read file if exist, create new if not */
+			const existing = await readOrCreateOutputFile(output_location);
+
 			/** ### ask user to select enpoint they will use */
-			const selectedPaths = await multiselect(
-				"Select endpoint you want to use",
-				{ choices: optionPaths, preSelectedChoices: [] }
-			);
-
-			/** ### filter object with selected path & method */
-			/** @type {import("openapi3-ts").oas30.PathObject} */
-			const selectedPathsDetail = {};
-			for (const item of selectedPaths) {
-				const [m, path] = item.split("  ");
-				const method = m.split("]")[0].slice(1).toLowerCase();
-
-				if (path in body.paths && method in body.paths[path]) {
-					selectedPathsDetail[path] = {};
-					selectedPathsDetail[path][method] = body.paths[path][method];
-				}
-			}
+			const selectedPathsDetail = getSelectedPathsPrompt(body, optionPaths);
 
 			/** ### parse selected endpoint to get related schema using $ref as reference */
-			const refs = JSON.stringify(selectedPathsDetail).split(`"$ref"`);
-
-			const schemas = new Set([]);
-			for (let i = 0; i < refs.length; i++) {
-				if (i === 0) continue;
-				let ref = refs[i].split("}")[0];
-				ref = ref.slice(2, ref.length - 1);
-
-				schemas.add(ref);
-			}
-
-			/** ### extract complete top-level schema value  */
-			/** @type {object} */
-			const schemasDetails = {};
-			for (const sc of schemas) {
-				const schemaName = sc.split("/").slice(1);
-				schemasDetails[schemaName[2]] =
-					body[schemaName[0]][schemaName[1]][schemaName[2]];
-			}
+			const schemasDetails = getSelectedPathRef(
+				body,
+				JSON.stringify(selectedPathsDetail)
+			);
 
 			/** find nested $ref and repeat the same process as above */
-			const nestedRefs = JSON.stringify(schemasDetails).split(`"$ref"`);
-
-			const nestedSchemas = new Set([]);
-			for (let i = 0; i < nestedRefs.length; i++) {
-				if (i === 0) continue;
-				let ref = nestedRefs[i].split("}")[0];
-				ref = ref.slice(2, ref.length - 1);
-
-				nestedSchemas.add(ref);
-			}
-
-			/** ### extract complete schema value  */
-			/** @type {object} */
-			const nestedSchemasDetails = {};
-			for (const sc of nestedSchemas) {
-				const schemaName = sc.split("/").slice(1);
-				nestedSchemasDetails[schemaName[2]] =
-					body[schemaName[0]][schemaName[1]][schemaName[2]];
-			}
-
-			console.log({ schemas, nestedSchemas });
+			const nestedSchemasDetails = getSelectedPathRef(
+				body,
+				JSON.stringify(schemasDetails)
+			);
 
 			console.log("\n");
 			console.log("Writing to the file...");
 
-			/** read file if exist, create new if not */
-			const e = await readOrCreateOutputFile(output_location);
-
 			// TODO: handle case when file already exist
-			if (e) {
+			if (existing) {
 			} else {
 				await fs.writeFile(
 					output_location,
@@ -170,3 +119,142 @@ import { readOrCreateOutputFile } from "./read-or-create-output.js";
 		exitWithError("Invalid URL. Try '--help' to learn more.");
 	}
 })();
+
+/**
+ * function to exit the program with optional error message
+ * @param {string} err
+ * @param {number} [exitCode]
+ * @returns void
+ */
+export function exitWithError(err, exitCode) {
+	if (err) console.error(err);
+	process.exit(exitCode);
+}
+
+/**
+ * Validate fetch response from URL provided
+ * @param {import('undici').Dispatcher.ResponseData} res
+ * @returns {Promise<import('openapi3-ts').oas30.OpenAPIObject>}
+ */
+export async function validateResponseFormat(res) {
+	/** exit when request failed */
+	if (res.statusCode < 200 || res.statusCode > 300) {
+		exitWithError(
+			`Invalid Request: Something went wrong, error ${res.statusCode}.`
+		);
+	}
+
+	/** check http header to make sure it's json file */
+	if (
+		"content-type" in res.headers &&
+		!res.headers["content-type"]?.includes("application/json")
+	) {
+		exitWithError("Invalid Format: URL is't returning valid JSON file.");
+	}
+
+	/**
+	 * parse content to make sure it's valid openAPI definition
+	 * by checking if required fields exist
+	 * references: https://swagger.io/specification/
+	 */
+
+	const body = /** @type {object} */ (await res.body.json());
+	if (!body.openapi || !body.info || !body.info.title || !body.info.version) {
+		exitWithError(
+			"Invalid Format: Use valid OpenAPI definition. Go to https://swagger.io/specification/ for reference. "
+		);
+	}
+
+	return body;
+}
+
+/**
+ *
+ * @param {import("openapi3-ts/oas30").OpenAPIObject} body
+ * @param {string[]} options
+ */
+async function getSelectedPathsPrompt(body, options) {
+	/** ### ask user to select enpoint they will use */
+	const selectedPaths = await multiselect("Select endpoint you want to use", {
+		choices: options,
+		preSelectedChoices: [],
+	});
+
+	/** ### filter object with selected path & method */
+	/** @type {import("openapi3-ts").oas30.PathObject} */
+	const selectedPathsDetail = {};
+	for (const item of selectedPaths) {
+		const [m, path] = item.split("  ");
+		const method = m.split("]")[0].slice(1).toLowerCase();
+
+		if (path in body.paths && method in body.paths[path]) {
+			selectedPathsDetail[path] = {};
+			selectedPathsDetail[path][method] = body.paths[path][method];
+		}
+	}
+
+	return selectedPathsDetail;
+}
+
+/**
+ *
+ * @param {import("openapi3-ts/oas30").OpenAPIObject} body
+ * @param {string} stringify
+ */
+function getSelectedPathRef(body, stringify) {
+	const refs = stringify.split(`"$ref"`);
+
+	const schemas = new Set([]);
+	for (let i = 0; i < refs.length; i++) {
+		if (i === 0) continue;
+		let ref = refs[i].split("}")[0];
+		ref = ref.slice(2, ref.length - 1);
+
+		schemas.add(ref);
+	}
+
+	/** ### extract complete top-level schema value  */
+	/** @type {object} */
+	const schemasDetails = {};
+	for (const sc of schemas) {
+		const schemaName = sc.split("/").slice(1);
+		schemasDetails[schemaName[2]] =
+			body[schemaName[0]][schemaName[1]][schemaName[2]];
+	}
+
+	return schemasDetails;
+}
+
+/**
+ * Check if provided file path exist, if not create new file.
+ * @param {string} path
+ */
+export async function readOrCreateOutputFile(path) {
+	try {
+		const r = await fs.readFile(path, "utf-8");
+		// TODO: handle if files exist
+		return r;
+	} catch (err) {
+		if ("code" in err && err.code === "ENOENT" && "message" in err) {
+			console.log("File not found, creating a new file...");
+
+			try {
+				const pathArr = path.split("/");
+				console.log({
+					pathArr,
+					dir: pathArr.slice(0, pathArr.length - 1).join("/"),
+					fil: pathArr[pathArr.length - 1],
+				});
+
+				await fs.mkdir(pathArr.slice(0, pathArr.length - 1).join("/"), {
+					recursive: true,
+				});
+
+				await fs.writeFile(path, "");
+				console.log("File created successfully.");
+			} catch (err) {
+				exitWithError(`Error creating file: ${err}`);
+			}
+		} else exitWithError(`Error reading file: ${err}`);
+	}
+}
