0489415b658bd4d287856476af9db6d9695bdf00
Khoirul Asfian <mk.asfian@gmail.com>
2025-07-27 12:05:41 +0700

    feat: add github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,12 @@
 </script>
 
 <header
-	class="container mx-auto flex h-10 items-center border-b border-solid border-neutral-200 text-lg font-semibold dark:border-neutral-700"
+	class="container mx-auto flex h-10 items-center justify-between border-b border-solid border-neutral-200 dark:border-neutral-700"
 >
-	<a href="/">Swagrec</a>
+	<a href="/" class="text-lg font-semibold">Swagrec</a>
+	<div class="flex items-center justify-end gap-x-2">
+		<a href="https://github.com/rulasfia/swagrec" class="font text-sm hover:underline"> Github </a>
+	</div>
 </header>
 
 {@render children()}
